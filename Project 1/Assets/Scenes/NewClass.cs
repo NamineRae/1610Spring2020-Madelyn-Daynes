c2d99cea40fbef8ce368b7b0b92a519c8a6bf5e2
Madelyn Daynes <maddy.daynes@gmail.com>
2020-01-15 11:01:27 -0700

    Variables Lecutre

--- Project 1/Assets/Scenes/NewClass.cs
+++ Project 1/Assets/Scenes/NewClass.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NewClass : MonoBehaviour
-{
+{ 
+    public string nameString;
+
+    public int intNum;
+
+    public float floatnum;
+
+    public GameObject gameobj;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        print(nameString);
+        print(intNum);
+        print(floatnum);
+        print(gameobj);
     }
 
     // Update is called once per frame
