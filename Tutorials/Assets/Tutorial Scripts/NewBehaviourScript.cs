1edf08f22bc96dc3ce9bc9566e6871c117f26da6
Madelyn Daynes <maddy.daynes@gmail.com>
2020-01-21 14:21:12 -0700

    Made Unity tutorial proj. & code to change an object's color

---
+++ Tutorials/Assets/Tutorial Scripts/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 'Assets.Tutorial_Scripts' : MonoBehaviour
+{
+    // This is the color changer
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>
+                ().material.color = Color.red;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>
+                ().material.color = Color.green;
+        }
+
+        if (Input.GetKeyDown(KeyCode.b))
+        {
+            GetCOmponent<Renderer>
+                ().material.color = Color.blue;
+        }
+    }
+}
+
+
+  
