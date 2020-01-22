dd3e3648482349bfb63ec7c21e2645ca1a526768
Madelyn Daynes <maddy.daynes@gmail.com>
2020-01-22 11:38:02 -0700

    Fixed RGBColorChanger

--- Tutorials/Assets/Tutorial Scripts/RGBColorChanger.cs
+++ Tutorials/Assets/Tutorial Scripts/RGBColorChanger.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 //This is from a Unity Learn tutorial
-public class ExampleBehaviorScript : MonoBehaviour
+public class RGBColorChanger : MonoBehaviour
 {
     void Update()
     {
