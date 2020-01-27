327e1b315bcf4c6f1c2dcaa3a66b9eacddf3aec8
Madelyn Daynes <maddy.daynes@gmail.com>
2020-01-27 12:57:06 -0700

    Started 8 tutorial scripts, finished 5

---
+++ Project 1/Assets/TutorialScripts/EnableComponentsTutorial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+public class EnableComponentsTutorial : MonoBehaviour
+{
+   private Light myLight;
+
+   private void Start()
+   {
+      myLight = GetComponent<Light>();
+   }
+
+   void Update()
+   {
+      if(Input.GetKeyUp(KeyCode.Space))
+      {
+      myLight.enabled = !myLight.enabled;
+      }
+   }
+}
