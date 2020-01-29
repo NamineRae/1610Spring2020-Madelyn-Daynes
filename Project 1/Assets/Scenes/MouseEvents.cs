9cb760625aba578fa9f71b0cce58a043d2e89b8d
Madelyn Daynes <maddy.daynes@gmail.com>
2020-01-29 11:08:35 -0700

    added mouse events

---
+++ Project 1/Assets/Scenes/MouseEvents.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.Events;
+
+public class MouseEvents : MonoBehaviour
+{
+    public UnityEvent mouseDownEvent, mouseUpEvent, mouseExitEvent, mouseEnterEvent, mouseDragEvent, mouseOverEvent;
+    private void OnMouseDown()
+    {
+    mouseDownEvent.Invoke();
+    }
+    
+    private void OnMouseUp()
+    {
+        mouseUpEvent.Invoke();
+    }
+
+    private void OnMouseExit()
+    {
+        mouseExitEvent.Invoke();
+    }
+    
+    private void OnMouseEnter()
+    {
+        mouseEnterEvent.Invoke();
+    }
+
+    private void OnMouseDrag()
+    {
+        mouseDragEvent.Invoke();
+    }
+
+    private void OnMouseOver()
+    {
+        mouseOverEvent.Invoke();
+    }
+}
