5d4fc21aa32e9549414ada1a757880f8f47de126
Madelyn Daynes <maddy.daynes@gmail.com>
2020-01-28 22:54:53 -0700

    Added cde for Stay and Exit. Couldn't get effects to work

--- Project 1/Assets/Scenes/TriggerEvents.cs
+++ Project 1/Assets/Scenes/TriggerEvents.cs
@@ -1,12 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Events;
 
 public class TriggerEvents : MonoBehaviour
 {
     public UnityEvent triggerEnterEvent;
-    
+
     private void OnTriggerEnter(Collider other)
     {
         triggerEnterEvent.Invoke();
+    }
+
+public UnityEvent triggerStayEvent;
+
+public void OnTriggerStay(Collider other)
+    {
+       triggerStayEvent.Invoke() ;
+    }
+
+public UnityEvent triggerExitEvent;
+    public void OnTriggerExit(Collider other)
+    {
+        triggerExitEvent.Invoke();
     }
 }
