fc73cef8aebc410bfc819f7fe5c1ccf3836b0be9
Madelyn Daynes <maddy.daynes@gmail.com>
2020-01-29 10:48:26 -0700

    Triggering Events/added actions to trigger events

---
+++ Project 1/Assets/Scenes/TriggeringEvents.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggeringEvents : MonoBehaviour
+{
+    private int triggerInteger;
+
+    private float triggerFloat;
+
+    private string triggerString;
+
+    public UnityEvent triggeringEnterEvent;
+
+    public void OnTriggerEnter(Collider other)
+    { 
+        triggeringEnterEvent.Invoke();
+    }
+
+    public UnityEvent triggeringStayEvent;
+
+    public void OnTriggerStay(Collider other)
+    {
+        triggeringStayEvent.Invoke();
+    }
+
+    public UnityEvent triggeringExitEvent;
+
+    public void OnTriggerExit(Collider other)
+    {
+        triggeringExitEvent.Invoke(); 
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
