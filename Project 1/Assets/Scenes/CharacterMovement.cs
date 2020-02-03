f085d3c1de1f7de94e30e193d92cdd9b53e061c1
Madelyn Daynes <maddy.daynes@gmail.com>
2020-02-03 11:27:47 -0700

    Character Controller Lecture 1

---
+++ Project 1/Assets/Scenes/CharacterMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 positionDirection;
+    public float speed = 5f;
+    public float gravity = -3f;
+    public float jumpForce = 10f; 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Vertical") * speed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            positionDirection.y = jumpForce; 
+        }
+        positionDirection.y += gravity;
+        controller.Move(positionDirection*Time.deltaTime);
+    }
+}
