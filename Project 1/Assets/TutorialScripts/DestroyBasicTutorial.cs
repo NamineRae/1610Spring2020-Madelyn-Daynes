76fab684b94a667d6480f72a6411da9484d6e43d
Madelyn Daynes <maddy.daynes@gmail.com>
2020-02-05 12:00:50 -0700

    Destroy Game Object tutorial

--- Project 1/Assets/TutorialScripts/DestroyBasicTutorial.cs
+++ Project 1/Assets/TutorialScripts/DestroyBasicTutorial.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class DestroyBasicTutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Destroy(gameObject);
+        }
     }
 }
+//from unity learn tutorial
--- Project 1/Assets/TutorialScripts/DestroyOtherTutorial.cs
+++ Project 1/Assets/TutorialScripts/DestroyOtherTutorial.cs
@@ -1,18 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class DestroyOtherTutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject other;
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Destroy(other);
+        }
     }
 }
+//from unity learn tutorial
