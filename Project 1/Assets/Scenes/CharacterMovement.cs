229c660c3a22e5a5fbca6916af9e5020327cef0f
Madelyn Daynes <maddy.daynes@gmail.com>
2020-02-10 11:30:58 -0700

    If statements, Rigidbody, Scope, component

--- Project 1/Assets/Scenes/CharacterMovement.cs
+++ Project 1/Assets/Scenes/CharacterMovement.cs
@@ -1,24 +1,34 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class CharacterMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    private CharacterController controller;
     public Vector3 positionDirection;
     public float speed = 10f;
     public float gravity = 3f;
-    public float jumpForce = 30f; 
+    public float jumpForce = 30f;
+    private int jumpCount = 0;
+    public int jumpCountMax = 2;
+    
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
+        if (controller.isGrounded)
+        {
+            positionDirection.y = 0;
+            jumpCount = 0;
+        }
         positionDirection.x = Input.GetAxis("Horizontal")*speed;
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
         {
             positionDirection.y = jumpForce;
+            jumpCount++;
         }
         controller.Move(positionDirection*Time.deltaTime);
         positionDirection.z = Input.GetAxis("Vertical")*speed;
---
+++ Project 1/Assets/Scenes/IfStatement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class IfStatement : MonoBehaviour
+{
+    public int a = 10;
+    public int b = 4;
+    private int c = 14;
+    public string password = "OU812";
+    public bool canRun = true;
+
+    void Start()
+    {
+        if (a + b == c)
+        {
+            print(message: "That is true");
+        }
+        
+        if (password != "OU812")
+        {
+            print(message: "That is the correct password.");
+        }
+        
+        if (!canRun)
+        {
+            print(message: "We can't run.");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
