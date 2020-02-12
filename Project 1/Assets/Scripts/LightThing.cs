1d0e17bf1559d640931e5d2132e17cc78a155484
Madelyn Daynes <maddy.daynes@gmail.com>
2020-02-12 11:28:03 -0700

    Components lecture
    I keep getting SO LOST in class! I'm trying my best but I still get SO CONFUSED

---
+++ Project 1/Assets/Scripts/LightThing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class LightThing : MonoBehaviour
+{
+    public Light Lightobj;
+ 
+    void Update()
+    {
+        
+        bool down = Input.GetKeyDown(KeyCode.P);
+        if (down)
+        {
+            Lightobj.GetComponent<Light>().color = new Color(255, 0, 242);
+        }
+    }
+}
+// code base found here https://answers.unity.com/questions/1211937/change-color-in-c-with-rgb-values.html
---
+++ Project 1/Assets/Scripts/SpriteController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro.SpriteAssetUtilities;
+using UnityEngine;
+[RequireComponent(typeof(Sprite))]
+public class SpriteController : MonoBehaviour
+{
+    public Color offColor = Color.cyan;
+    public Color SpriteColor = Color.magenta;
+    private SpriteRenderer spriteObj;
+    void Start()
+    {
+        spriteObj = GetComponent<SpriteRenderer>();
+        spriteObj.color = SpriteColor;
+    }
+
+    private void OnEnable()
+    {
+        spriteObj.color = SpriteColor;
+    }
+
+    private void OnDisable()
+    {
+        spriteObj.color = offColor;
+    }
+}
