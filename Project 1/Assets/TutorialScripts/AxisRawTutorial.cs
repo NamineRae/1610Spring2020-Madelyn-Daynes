119c4685a6bee319e1e80f3534541a7d1bec8488
Madelyn Daynes <maddy.daynes@gmail.com>
2020-02-13 17:29:45 -0700

    Axis tutorial
    I don't know how this is supposed to work, but I wrote the code

---
+++ Project 1/Assets/TutorialScripts/AxisRawTutorial.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisRawTutorial : MonoBehaviour
+{
+    public float range;
+    public GUIText textOutput;
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float xPos = h * range;
+
+        transform.position = new Vector3(xPos, 2f, 0);
+        textOutput.text = "Value Returned: " + h.ToString("F2");
+    }
+}
+//unity learn tutorial 
---
+++ Project 1/Assets/TutorialScripts/AxisTutorial.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AxisTutorial : MonoBehaviour
+{
+    public float range;
+    public GUIText textOutput;
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float xPos = h * range;
+        
+        transform.position = new Vector3(xPos, 2f, 0);
+        textOutput.text = "Value Returned: "+h.ToString("F2");
+    }
+}
+//unity learn tutorial
