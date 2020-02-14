26d9fe54ba149ebb5f7231e7d4d0947cc7ca275a
Madelyn Daynes <maddy.daynes@gmail.com>
2020-02-13 17:45:56 -0700

    Mouse Click Tutorial
    Code written, but i'm not sure what it does

---
+++ Project 1/Assets/TutorialScripts/MouseClickTutorial.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClickTutorial : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().AddForce(-transform.forward * 500f);
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}
+//unity learn tutorial
