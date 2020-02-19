e2f348d6630d5e1ac7c4bfd8345c3de8b8d6f382
Madelyn Daynes <maddy.daynes@gmail.com>
2020-02-19 11:31:58 -0700

    Scriptable Objects

---
+++ Project 1/Assets/Scripts/FloatData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+  public float value = 1f;
+
+  public void ChangeCalue(float number)
+  {
+    value += number;
+  }
+}
+//good for keeping track of health points
---
+++ Project 1/Assets/Scripts/ImageBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageBehaviour : MonoBehaviour
+{
+    private Image imageObj;
+    void Start()
+    {
+        imageObj = GetComponent<Image>();
+    }
+    
+    void Update()
+    {
+        
+    }
+}
+//hw= try to get health image to reflect the float value
