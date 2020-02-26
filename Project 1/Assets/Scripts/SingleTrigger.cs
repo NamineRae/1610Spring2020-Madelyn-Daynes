07c1765a66c656b6f393020bc2baa5ad836d2e94
Madelyn Daynes <maddy.daynes@gmail.com>
2020-02-25 17:03:28 -0700

    Help. Please. I can't get the health bar to work.

---
+++ Project 1/Assets/Scripts/SingleTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class SingleTrigger : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent;
+
+    private void OnTriggerEnter (Collider other)
+    { 
+        triggerEnterEvent.Invoke();
+    }
+}
