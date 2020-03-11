0e9c77c0659499d7b4c4ccc9a87d2f8f976eacbc
Madelyn Daynes <maddy.daynes@gmail.com>
2020-03-11 11:26:51 -0600

    Loops, lists, arrays lecture

---
+++ Project 1/Assets/Scripts/Collectable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro.SpriteAssetUtilities;
+using UnityEngine;
+[CreateAssetMenu]
+public class Collectable : MonoBehaviour
+{
+    public int powerLevel = 9001;
+    public Color collectableColor = Color.cyan;
+    public Sprite
+}
