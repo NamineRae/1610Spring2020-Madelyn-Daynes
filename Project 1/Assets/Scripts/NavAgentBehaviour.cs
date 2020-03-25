f6d3b41abb4bf4dfb2ba77da052fcadf7e9bcb24
Madelyn Daynes <maddy.daynes@gmail.com>
2020-03-25 15:18:24 -0700

    AI lecture

---
+++ Project 1/Assets/Scripts/NavAgentBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavAgentBehaviour : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player;
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    void Update()
+    {
+        agent.destination = PlayerPrefs.position;
+
+    }
+}
