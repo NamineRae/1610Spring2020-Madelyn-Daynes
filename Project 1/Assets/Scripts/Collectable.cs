6e60f7170c79dc80574f57aba3c470d2e2a5c14a
Madelyn Daynes <maddy.daynes@gmail.com>
2020-04-05 20:30:35 -0700

    Got camera to follow player. Tried to fix warnings but have tech issues
    unity had to import small assets every five seconds.

--- Project 1/Assets/Scripts/Collectable.cs
+++ Project 1/Assets/Scripts/Collectable.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using TMPro.SpriteAssetUtilities;
 using UnityEngine;
 [CreateAssetMenu]
-public class Collectable : MonoBehaviour
+public class Collectable : ScriptableObject
 {
     public int powerLevel = 9001;
     public Color collectableColor = Color.cyan;
