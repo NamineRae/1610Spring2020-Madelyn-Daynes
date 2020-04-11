e6fda20cac56f7b64f7b05e21109e55706d502c1
Madelyn Daynes <maddy.daynes@gmail.com>
2020-04-11 16:43:15 -0700

    Had a second tutoring session, learned how to add art to game and more

---
+++ Project 1/Assets/Scripts/Functions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Functions : MonoBehaviour
+{
+    public void Add(int a, int b)
+    {
+        int c = a + b;
+        print(c);
+    }
+
+    public int AddWithReturn(int a, int b)
+    {
+        return a + b;
+    }
+
+    void Start()
+    {
+        int c = AddWithReturn(7, 2);
+        Add(9, 7);
+    }
+}
---
+++ Project 1/Assets/Scripts/WhileIfElse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileIfElse : MonoBehaviour
+{
+    void Start()
+    {
+        int counter = 10;
+        while (counter > 0)
+        {
+            if(counter % 2 = 0)
+            {
+                print(counter + "is even");
+            }
+            else
+            {
+                print(counter + " is odd");
+            }
+
+            counter = counter - 1;
+        }
+
+    }
+   
+}
